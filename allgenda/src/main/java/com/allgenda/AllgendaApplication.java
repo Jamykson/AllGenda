45fd1ae98c08b465842d4763292465d170f6783a
gabri <gabrielnicolas253@gmail.com>
2026-09-14 12:46:42 -0300

    estrutura inicial do projeto baseada em spring boot

---
+++ allgenda/src/main/java/com/allgenda/AllgendaApplication.java
@@ -0,0 +1,13 @@
+package com.allgenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllgendaApplication.class, args);
+	}
+
+}
---
+++ allgenda/src/test/java/com/allgenda/AllgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allgenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
